b080a5d0ae401f1f540e6f579bf6fdb7d526884c
jahson123 <74171442+jahson123@users.noreply.github.com>
2022-05-31 23:43:25 +0800

    Update Guess num.py

--- Guess num.py
+++ Guess num.py
@@ -1,16 +1,37 @@
-y = 0
+import random
 
-
-while y < 3 :
-    guess = int(input("Guess: "))
-    y += 1
-    if guess == 9:
-        print("You won")
+while True:
+    y = 0
+    a = random.randint(1,9)
+    print(a)
+    while y < y+1:
+        n = int(input("Guess 1 to 9: "))
+        y +=1
+        if n < a:
+            print("Too low")
+        elif n > a:
+            print("Too high")
+        else:
+            print("Correct")
+            print(f'You have guess: {y} times')
+            break
+    say = input("Play again? Yes/No ")
+    if say.lower() == "no":
+        print("Thank You for playing")
         break
-else:
-    print("You lose")
     
+def guess():
     
-    
-    
-    
+    y = random.randint(1, 10)
+    print(y)
+    while True:
+        x = int(input("Guess: "))
+        if x > y:
+            print("Too high")
+        elif x < y:
+            print("Too low")
+        else:
+            print("Correct")
+            return True
+
+guess()
